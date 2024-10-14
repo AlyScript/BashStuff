5b911cbff1ef7f93268cf1c0b72a4f8b4a13b10c
adam <adamaly93@gmail.com>
2024-10-14 14:27:37 +0100

    Modified check for changes on remote.

--- functions.sh
+++ functions.sh
@@ -5,12 +5,7 @@
 function check_git_updates() {
     if git rev-parse --is-inside-work-tree > /dev/null 2>&1
     then
-        git fetch &> /dev/null
-
-        LOCAL=$(git rev-parse @) # '@' is shorthand for the HEAD of current branch
-        BASE=$(git merge-base @ @{u}) # '@{u}' is shorthand for the upstream branch
-        
-        if [ "$LOCAL" = "$BASE" ]
+        if [ $(git fetch 2>&1 | wc -c) -ge 1 ]
         then
             read -p "Your branch is behind the remote branch. Do you want to pull the latest changes? (y/n): " answer
             if [ "$answer" = "y" ]
