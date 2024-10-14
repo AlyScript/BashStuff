14cc1d067b839f2f9198d009739ed6cc4969016e
adam <adamaly93@gmail.com>
2024-10-14 13:38:05 +0100

    Function to pull latest changes everytime you enter a repository.

---
+++ functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Function to ask if user wants to pull latest changes from git. This should be added to your .bashrc or .bash_profile.
+
+function check_git_updates() {
+    if res-parse --is-inside-work-tree
+    then
+        git fetch &> /dev/null
+
+        LOCAL=$(git rev-parse @) # '@' is shorthand for the HEAD of current branch
+        BASE=$(git merge-base @ @{u}) # '@{u}' is shorthand for the upstream branch
+        
+        if [ "$LOCAL" = "$BASE" ]
+            read -p "Your branch is behind the remote branch. Do you want to pull the latest changes? (y/n): " answer
+            if [ "$answer" = "y" ]
+            then
+                git pull
+            else
+                echo "Skipping git pull"
+            fi
+        fi
+    fi
+}
