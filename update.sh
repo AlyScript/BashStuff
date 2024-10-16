8330afe90e987e9ea6527400aa6181de90e0f5a2
AlyScript <adamaly93@gmail.com>
2024-10-16 18:50:53 +0100

    Automated update script.

--- update.sh
+++ update.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
 
 if grep -q "Arch" $release_file
 then
     # The host is based on Arch, run the pacman command
-    sudo pacman -Syu
+    sudo pacman -Syu 1>>$logfile 2>>$errorlog
+    if [ %? -ne 0 ]
+    then
+	echo "An error occured, please check the $errorlog file."
+    fi
 fi
 
 if grep -q "Pop" $release_file || grep -q "Ubuntu" $release_file
 then
     # The host is based on Debian or Ubuntu
     # Run the apt version of the command
-    sudo apt update
-    sudo apt dist-upgrade
+    sudo apt update 1>>$logfile 2>>$errorlog 
+    if [ $? -ne 0 ]
+    then
+	echo "An error occured, please check the $errorlog file."
+    fi
+    sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
+    if [ $? -ne 0 ]
+    then
+	echo "An error occured, please check the $errorlog file."
+    fi
 fi
