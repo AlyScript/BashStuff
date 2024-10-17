be631f510cf35967c376e65924cf2b46db8d9813
AlyScript <adamaly93@gmail.com>
2024-10-17 19:12:23 +0100

    Counting directory objects.

---
+++ directorycount.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -lt 1  ]
+then
+    echo "Not enough arguments!"
+    echo "Try again, ensuring you pass in at least one directory."
+    exit 1
+fi
+
+lines=$(ls -lh $1 | wc -l)
+
+echo "You have $(($lines-1)) objects in the $1 directory."
